a07fe47a69639a4919c3e7e9f61562ac75a9382f
Gonzalo <arguellogonzalo97@gmail.com>
2023-05-25 17:02:23 -0300

    Login mobile V1.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import OnBoarding from "./pages/OnBoarding";
+import Login from "./pages/Login"
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<OnBoarding />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
