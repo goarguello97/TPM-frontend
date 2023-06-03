381748e7d20b1ff6a8844b8d237743bb73a1d35c
Gonzalo <arguellogonzalo97@gmail.com>
2023-06-03 16:51:10 -0300

    OnBoarding desktop in progress

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,17 +2,19 @@ import Info from "../../assets/img/Navbar/Info.svg";
 import Statics from "../../assets/img/Navbar/Statics.svg";
 import User from "../../assets/img/Navbar/User.svg";
 import Users from "../../assets/img/Navbar/Users.svg";
-import UsersActive from "../../assets/img/Navbar/UsersActive.svg"
+import UsersActive from "../../assets/img/Navbar/UsersActive.svg";
+import useMediaQuery from "../../hooks/useMediaQuery";
 
 const Navbar = () => {
-  return (
+  const { width } = useMediaQuery();
+  return width < 1024 ? (
     <div className="fixed bottom-[0px] w-[100vw] h-[82px] fold-horizontal:h-[47px] bg-navbar rounded-t-[40px] flex justify-between z-10">
       <img src={UsersActive} alt="UsersActive" className="w-[20px] ms-[67px]" />
       <img src={Statics} alt="Statics" className="w-[20px]" />
       <img src={Info} alt="Info" className="w-[20px]" />
       <img src={User} alt="User" className="w-[20px] me-[67px]" />
     </div>
-  );
+  ) : null;
 };
 
 export default Navbar;
--- src/pages/OnBoarding.tsx
+++ src/pages/OnBoarding.tsx
@@ -1,10 +1,13 @@
 import Logo from "../assets/img/ThePerfectMentor.svg";
 import Ellipse from "../assets/img/Ellipse.svg";
-import Doodle from "../assets/img/Doodle.svg"
-import Doodle2 from "../assets/img/Doodle2.svg"
+import Doodle from "../assets/img/Doodle.svg";
+import Doodle2 from "../assets/img/Doodle2.svg";
+import Doodle3 from "../assets/img/Doodle3.svg";
+import useMediaQuery from "../hooks/useMediaQuery";
 
 const OnBoarding = () => {
-  return (
+  const { width } = useMediaQuery();
+  return width < 1024 ? (
     <div className="min-h-100% bg-background relative">
       <div className="absolute w-[100vw] h-[511px] bg-saly bg-center bg-no-repeat top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
         <img
@@ -12,13 +15,21 @@ const OnBoarding = () => {
           src={Logo}
           alt="The Perfect Mentor"
         />
-        <img className="absolute w-[178.69px] top-[84px] z-10" src={Doodle} alt="Doodle" />
+        <img
+          className="absolute w-[178.69px] top-[84px] z-10"
+          src={Doodle}
+          alt="Doodle"
+        />
         <img
           className="absolute top-[59px] left-[66px] z-1"
           src={Ellipse}
           alt="Ellipse"
         />
-        <img className="absolute w-[130.86px] bottom-[18.57px] left-[104px]" src={Doodle2} alt="Doodle2" />
+        <img
+          className="absolute w-[130.86px] bottom-[18.57px] left-[104px]"
+          src={Doodle2}
+          alt="Doodle2"
+        />
       </div>
       <button className="absolute w-button h-[55px] bottom-[136px] left-[50%] translate-x-[-50%] bg-button rounded-[40px] font-bold text-[15px] text-[#fff]">
         Sign up
@@ -26,6 +37,36 @@ const OnBoarding = () => {
       <button className="absolute w-button h-[55px] bottom-[71px] left-[50%] translate-x-[-50%] border-[2px] border-button rounded-[40px] font-bold text-[15px]">
         Log In
       </button>
+    </div>
+  ) : (
+    <div className="w-[100vw] h-[100vh] bg-background flex justify-center items-center relative z-0">
+      <div className="w-[784.44px] h-[784.44px] bg-saly bg-no-repeat bg-center bg-contain absolute left-[35%] top-[48.5%] translate-x-[-50%] translate-y-[-50%] z-30"></div>
+      <div className="w-container-desktop h-[514px] bg-transparent border-[2px] border-[#444444] rounded-[40px] relative flex justify-end items-center z-10">
+        <img
+          src={Doodle}
+          alt="Doodle"
+          className="w-[221.08px] absolute left-[1.5%] top-[30%] translate-x-[-50%] translate-y-[-50%]"
+        />
+        <img
+          src={Doodle3}
+          alt="Doodle3"
+          className="w-[150px] absolute rotate-45 left-[44%] top-[-10%] translate-x-[-50%] translate-y-[-50%]"
+        />
+        <img
+          src={Doodle2}
+          alt="Doodle2"
+          className="w-[245px] h-[250px] absolute rotate-45 left-[100%] top-[18%] translate-x-[-50%] translate-y-[-50%]"
+        />
+        <div className="w-[50%] h-auto flex flex-col justify-center items-center">
+          <img src={Logo} alt="Logo" className="w-[248.37px] mb-[47.48px]" />
+          <button className="min-w-[175px] xl:min-w-[315px] h-[55px] bg-button rounded-[40px] font-bold text-[15px] text-[#fff] mb-[15px]">
+            Sign up
+          </button>
+          <button className="min-w-[175px] xl:min-w-[315px] h-[55px] border-[2px] border-button rounded-[40px] font-bold text-[15px]">
+            Log In
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
